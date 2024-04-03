c5a57db2063c4382355c97404d08712f0648dbdc
VLstack <github@vlstack.fr>
2024-04-03 08:22:17 +0200

    add documentation

--- Sources/VLKeyboardKit/VLKeyboard.swift
+++ Sources/VLKeyboardKit/VLKeyboard.swift
@@ -1,19 +1,28 @@
 import UIKit
 import Observation
 
+/**
+ `VLKeyboard` is a SwiftUI custom keyboard input view controller that extends `UIInputViewController`.
+ It provides functionality for managing a custom keyboard input view to use with TextField.
+*/
 @Observable
 public class VLKeyboard: UIInputViewController
 {
+ /// A closure type for handling feedback events.
  public typealias FeedbackHandler = () -> ()
  
+ /// The delegate for text field interactions.
  public var delegate: UITextFieldDelegate? = nil
 
+ /// The custom keyboard input view.
  @ObservationIgnored
  public private(set) lazy var keyboardInputView = VLKeyboard.InputView(keyboardView)
  
+ /// The underlying keyboard view.
  @ObservationIgnored
  internal var keyboardView: UIView! = nil
 
+ /// The feedback closure for input events.
  @ObservationIgnored
  internal var feedback: FeedbackHandler? = UIDevice.current.playInputClick
  
